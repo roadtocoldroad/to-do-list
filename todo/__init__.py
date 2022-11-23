925ff903443df41d4ff497c2df8b1d4118b118a7
parkcoldroad <coldstreet@naver.com>
2022-11-24 01:41:32 +0900

    refactor: Hide a credential to use env variable

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from . import db
 from todo.routes import bp
+import os
 
 def create_app():
     app = Flask(__name__)
 
-    app.secret_key = 'super secret key'
+    app.secret_key = os.environ.get("to_do_list_key")
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['DATABASE'] = 'todo/database.db'
 
