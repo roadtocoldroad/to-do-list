c919f36cdc0a40175138a60e8000261a8535af6c
parkcoldroad <coldstreet@naver.com>
2022-11-23 23:17:52 +0900

    refactor: Create routes.py to administrate url routing

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from todo.app import bp
+from todo.routes import bp
 
 def create_app():
     app = Flask(__name__)
--- todo/routes.py
+++ todo/routes.py
@@ -5,19 +5,22 @@ from . import db
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/',methods = ['POST','GET'])
-def index():
-    if request.method == 'POST':
+@bp.route('/',methods = ['GET'])
+def read():
+    return db.read_data("select * from todo",'')
+
+
+@bp.route('/',methods = ['POST'])
+def post():
         title = request.form['title']
         url = request.url
         con = sql.connect('database.db')
         cur = con.cursor()
-        cur.execute("insert into todo(TITLE,URL) values (?,?)", (title, url))
+        cur.execute("insert into todo(title,url) values (?,?)", (title, url))
         con.commit()
-        return redirect(url_for("main.index"))
-    return db.read_data("select * from todo",'')
+        return redirect(url_for("main.post"))
 
 
 @bp.route('/<string:todo_id>')
-def read_by_id(todo_id):
+def find_todo_by_id(todo_id):
    return db.read_data("select * from todo where todo_id=?", todo_id)
